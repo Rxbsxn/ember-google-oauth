2b98e2369092e526b51fa7216f03768fb2a332c1
Robert Harężlak <harezlak.robert7@gmail.com>
2017-09-06 03:02:34 +0200

    Login component

--- app/components/login-component/component.js
+++ app/components/login-component/component.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {
+  inject,
+  Component,
+  computed
+} = Ember;
+
+export default Component.extend({
+  auth: inject.service(),
+  session: inject.service(),
+  profileName: computed.alias('session.data.authenticated.profile.name'),
+  profilePict: computed.alias('session.data.authenticated.profile.picture'),
+
+  actions: {
+    login() {
+      this.get('auth').login();
+    },
+
+    logout() {
+      this.get('auth').logout();
+    }
+  }
 });
